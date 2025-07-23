15c9facb896640e056c4515d037680c7b5ec3d72
Mahmoud Otefy <mahmoud@example.com>
2025-07-24 02:40:10 +0300

    adding some files

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -11,7 +11,7 @@ import { Component } from '@angular/core';
 export class Projects {
   projects = [
     {
-      title: 'Project One',
+      title: 'custmize-psd',
       description:
         'A responsive website built with Html and css to showcase awesome stuff.',
       image: 'assets/images/project1.png',
@@ -19,11 +19,25 @@ export class Projects {
       liveDemo: 'https://project1-live-demo.com',
     },
     {
-      title: 'Project Two',
-      description:
-        'E-commerce web app with real-time cart and payment integration.',
+      title: ' Agency-Landing-Page',
+      description: 'static site with an HtMl, CSS, Bootstarp',
       image: 'assets/images/project2.png',
       github: 'https://m-otefy.github.io/Agency-Landing-Page/',
+      liveDemo: 'https://project2-live-demo.com',
+    },
+    {
+      title: 'FakeApp Store',
+      description:
+        'A responsive e-commerce product page built with Angular. It fetches products from a fake store API and displays them in styled Bootstrap cards. Each product includes an image, title, price, and an "Add to Cart" button. The design is mobile-friendly, fast-loading, and follows Angular best practices. Deployed using GitHub Pages.',
+      image: 'assets/images/project2.png',
+      github: 'https://m-otefy.github.io/E-Shop/',
+      liveDemo: 'https://project2-live-demo.com',
+    },
+    {
+      title: 'custmize-psd',
+      description: 'static site with an HtMl, CSS, Bootstarp, SCSS',
+      image: 'assets/images/project2.png',
+      github: 'https://m-otefy.github.io/task-scss/',
       liveDemo: 'https://project2-live-demo.com',
     },
   ];
